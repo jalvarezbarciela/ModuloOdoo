b96eb02ac4fe946ae5b33c5ffd1058b73449742a
jalvarezbarciela <jalvarezbarciela@danielcastelao.org>
2021-03-16 21:40:31 +0100

    Models modificado

--- addons/openacademy/models/models.py
+++ addons/openacademy/models/models.py
@@ -1,18 +1,11 @@
 # -*- coding: utf-8 -*-
 
-# from odoo import models, fields, api
+from odoo import models, fields, api
 
 
-# class openacademy(models.Model):
-#     _name = 'openacademy.openacademy'
-#     _description = 'openacademy.openacademy'
+class Course(models.Model):
+    _name = 'openacademy.course'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+
+    name = fields.Char(string="Title",required=True)
+    description = fields.Text()
