17662899a9d5c3bb42ff9939a62697bffa6a7933
jalvarezbarciela <jalvarezbarciela@danielcastelao.org>
2021-03-24 12:55:52 +0100

    Security arreglado

--- addons/openacademy/__manifest__.py
+++ addons/openacademy/__manifest__.py
@@ -22,8 +22,9 @@
 
     # always loaded
     'data': [
-        #'security/ir.model.access.csv',
-        #'views/views.xml',
+        'security/ir.model.access.csv',
+        'security/security.xml',
+        'views/views.xml',
         'views/templates.xml',
         'views/openacademy.xml',
         'views/partner.xml',
